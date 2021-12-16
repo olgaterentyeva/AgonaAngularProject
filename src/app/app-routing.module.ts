e6e81b5cfa9b1eae7cd42ab5296a2f2c35d37276
Liaisan Akhmetova <akhmetova1423@gmail.com>
2021-12-16 17:41:48 +0300

    toggle, start doing film component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AboutFilmComponent} from "./pages/about-film/about-film.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:AboutFilmComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutFilmComponent } from './pages/about-film/about-film.component';
+import { ToggleComponent } from './components/toggle/toggle.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from "@angular/material/icon";
+import { InputComponent } from './components/input/input.component';
+import {MatButtonModule} from "@angular/material/button";
+import { FilmComponent } from './components/film/film.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutFilmComponent,
+    ToggleComponent,
+    InputComponent,
+    FilmComponent,
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
